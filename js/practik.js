28c9c60fbe3fd902a61ffebfb093db68128a0f26
ILYA <wahtang505@gmail.com>
2020-03-30 16:15:15 +0300

    Третий комит

--- js/practik.js
+++ js/practik.js
@@ -1,5 +1,14 @@
-let money = +prompt("Ваш бюджет на месяц?",""),
+ let money, time;
+
+ function start() {
+ 	 money = +prompt("Ваш бюджет на месяц?","");
 	 time = prompt("Введите дату в формате YYYY-MM-DD","");
+
+	 while (isNaN(money) || money == "" || money == null) {
+	 	money = +prompt("Ваш бюджет на месяц?","");
+	 }
+}
+start();
 
 let appData = {
 	budget: money,
@@ -7,22 +16,27 @@ let appData = {
 	optionalExpenses: {},
 	income: {},
 	timeData: time,
-	savings : false
+	savings : true
 };
 
-for (let i = 0; i < 2; i++) {
-	let a = prompt('Введите обязательную статью расходов в этом месяце',''),
-		b = prompt('Во сколько обойдется?','');
-	if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
-		&& a != '' && b != '' && a.lenght < 50){
-		console.log('done');
-		appData,exprenses[a] = b;
-	} else {
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt('Введите обязательную статью расходов в этом месяце',''),
+			 b = prompt('Во сколько обойдется?','');
 
+		if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
+			&& a != '' && b != '' && a.lenght < 50) {
+			console.log('done');
+			appData.exprenses[a] = b;
+		} else {
+			
+		}
 	}
-};
+}
 
-appData.moneyPerDay = appData.budget / 30;
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
 
 alert('Ежедневный бюджет:' + appData.moneyPerDay);
 
@@ -36,3 +50,14 @@ if (appData.moneyPerDay < 100) {
 	console.log('Произошла ошибка')
 }
 
+function checkSavings(){
+	if (appData.savings == true) {
+		let save = +prompt('Какова сумма накоплений?','');
+		perсent = +prompt('Под какой процент?','');
+
+		appData.monthIncome = (save/100/12*perсent).toFixed(1);
+		alert('Доход с вашего депозита' + appData.monthIncome);
+	}
+}
+
+checkSavings();
